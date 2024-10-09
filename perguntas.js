0bb6bcb87db629edf33e9a3594db73ffa93bcb18
hellihub <hellen.goes@escola.pr.gov.br>
2024-10-09 11:05:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Chimborazo'
+)
+
+criaCartao(
+    'História',
+    'Quem inventou a lâmpada?',
+    'Thomas Edison'
+)
+
+criaCartao(
+    'Ciẽncias',
+    'Quais são as fases da Lua??',
+    'Nova, crescente, cheia e minguante'
+)
+
+criaCartao(
+    'Biologia',
+    'Quantos ossos temos no nosso corpo',
+    '206'
+)
